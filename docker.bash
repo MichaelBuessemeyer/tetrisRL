1e4194611391bf8a61326e0b5da7aedec89b5aff
Michael Buessemeyer <Michael.Buessemeyer@delos.eaalab.hpi.uni-potsdam.de>
2021-06-22 16:44:51 +0200

    update docker script

--- docker.bash
+++ docker.bash
@@ -3,11 +3,11 @@ if [[ $1 == "build" ]]; then
   echo "Currently not supported, using build image from the snake group"
   # docker build . -t rlearning-snake:latest --label tetris-rl-container-snake-clone 
 elif [[ $1 == "start" ]]; then
-  docker run -it -d --gpus 1 --name tetris-rl-container-snake-clone -v /home/Michael.Buessemeyer/tetrisRL:/tetrisRL rlearning-snake:latest
+  docker run -it -d --gpus 1 --name tetris-rl-container-snake-clone -v /home/Michael.Buessemeyer/tetrisRL:/tetrisRL -w /tetrisRL rlearning-snake:latest
 elif [[ $1 == "exec" && $2 == "-d" ]]; then
-  docker exec -it -d tetris-rl-container-snake-clone ${@:3}
+  docker exec -it -d --user Michael.Buessemeyer tetris-rl-container-snake-clone ${@:3}
 elif [[ $1 == "exec" ]]; then
-  docker exec -it tetris-rl-container-snake-clone ${@:2} 
+  docker exec -it --user Michael.Buessemeyer tetris-rl-container-snake-clone ${@:2} 
 elif [[ $1 == "rm" ]]; then
   docker stop tetris-rl-container-snake-clone && docker rm tetris-rl-container-snake-clone
 fi
