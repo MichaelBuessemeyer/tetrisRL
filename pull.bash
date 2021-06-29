6ad41af8f0d4b4a7ff6e259251394775f6e2c34c
Michael Büßemeyer <michael.buessemeyer@student.hpi.de>
2021-06-29 17:08:56 +0200

    remove pull and push script

--- pull.bash
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-scp -r Michael.Buessemeyer@delos.eaalab.hpi.uni-potsdam.de:/home/Michael.Buessemeyer/tetrisRL/* /home/michael/Documents/uni-sachen/Master/Reinforcement_Learning/tetrisRL/
--- push.bash
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-scp -r /home/michael/Documents/uni-sachen/Master/Reinforcement_Learning/tetrisRL/* Michael.Buessemeyer@delos.eaalab.hpi.uni-potsdam.de:/home/Michael.Buessemeyer/tetrisRL/
