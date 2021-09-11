9fb1348adadaa9733d563b56f9976f32e3b70173
Clara Uktar <clara.uktar@student.hpi.uni-potsdam.de>
2021-09-11 18:48:11 +0200

    Add evaluation

--- rainbow/rainbow_main.py
+++ rainbow/rainbow_main.py
@@ -10,6 +10,7 @@ import pickle
 import numpy as np
 import torch
 from tqdm import trange
+import tensorflow as tf
 
 import os, sys
 currentdir = os.path.dirname(os.path.realpath(__file__))
@@ -63,8 +64,14 @@ parser.add_argument('--checkpoint-interval', default=0, help='How often to check
 parser.add_argument('--memory', help='Path to save/load the memory from')
 parser.add_argument('--disable-bzip-memory', action='store_true', help='Don\'t zip the memory file. Not recommended (zipping is a bit slower and much, much smaller)')
 
+parser.add_argument("--trainings_name", type=str, default="default", help="The time to wait after each env render.")
+
 # Setup
 args = parser.parse_args()
+
+# Constants
+num_eval_episodes = 50
+eval_interval = 2000
 
 print(' ' * 26 + 'Options')
 for k, v in vars(args).items():
@@ -87,6 +94,15 @@ else:
 def log(s):
   print('[' + str(datetime.now().strftime('%Y-%m-%dT%H:%M:%S')) + '] ' + s)
 
+# Logging Setup
+current_time = datetime.now().strftime("%Y_%m_%d-%H:%M:%S")
+train_log_dir = 'tensorboard/rainbow/' + args.trainings_name + "_" + current_time + '/train'
+test_log_dir = 'tensorboard/rainbow/' + args.trainings_name + "_" + current_time + '/test'
+train_summary_writer = tf.summary.create_file_writer(train_log_dir)
+test_summary_writer = tf.summary.create_file_writer(test_log_dir)
+running_reward = 0
+episode_reward_history = []
+
 
 def load_memory(memory_path, disable_bzip):
   if disable_bzip:
@@ -105,16 +121,43 @@ def save_memory(memory, memory_path, disable_bzip):
     with bz2.open(memory_path, 'wb') as zipped_pickle_file:
       pickle.dump(memory, zipped_pickle_file)
 
+def compute_avg_return(model, environment, num_episodes=10):
+
+  total_return = 0.0
+  for _ in range(num_episodes):
+      state = environment.do_reset()
+      episode_return = 0.0
+      done = False
+      counter = 0
+      MAX_EPISODE_LENGTH = 200
+      while not done and counter < MAX_EPISODE_LENGTH:
+          # Predict action Q-values
+          # From environment state
+          # state_tensor = tf.convert_to_tensor(state, dtype=tf.uint16)
+          # state_tensor = tf.expand_dims(state_tensor, 0)
+          action_probs = model.act(state)
+          # Take best action
+          action = np.argmax(action_probs[0]).numpy()
+
+          state, reward, done, _ = environment._step(action)
+          episode_return += reward
+          counter += 1
+      total_return += episode_return
+
+  avg_return = total_return / num_episodes
+  return avg_return
+
 
 # Environment
 width, height = 10, 20 # standard tetris friends rules
-env = TetrisEngine(width, height)
+train_env = TetrisEngine(width, height)
+test_env = TetrisEngine(width, height)
 # env.train()
 # action_space = env.action_space()
-action_space = env.get_action_count()
+action_space = train_env.get_action_count()
 
 # Agent
-dqn = Agent(args, env)
+dqn = Agent(args, train_env)
 
 # If a model is provided, and evaluate is false, presumably we want to resume, so try to load memory
 if args.model is not None and not args.evaluate:
@@ -136,9 +179,9 @@ val_mem = ReplayMemory(args, args.evaluation_size)
 T, done = 0, True
 while T < args.evaluation_size:
   if done:
-    state = env.do_reset()
+    state = test_env.do_reset()
 
-  next_state, _, done, _ = env._step(np.random.randint(0, action_space))
+  next_state, _, done, _ = train_env._step(np.random.randint(0, action_space))
   val_mem.append(state, -1, 0.0, done)
   state = next_state
   T += 1
@@ -150,20 +193,35 @@ if False:
   print('Avg. reward: ' + str(avg_reward) + ' | Avg. Q: ' + str(avg_Q))
 else:
   # Training loop
-  # dqn.train()
-  T, done = 0, True
+  dqn.train()
+  T, done = 0, False
+  state = train_env.do_reset()
+  episode_reward = 0
+  total_cleared_lines = 0
   for T in trange(1, args.T_max + 1):
     if done:
-      state = env.do_reset()
+      with train_summary_writer.as_default():
+        tf.summary.scalar('return', running_reward, step=T)
+        tf.summary.scalar("cleared_lines_count", total_cleared_lines, step=T)
+      state = train_env.do_reset()
+      episode_reward = 0
+      total_cleared_lines = 0
 
     if T % args.replay_frequency == 0:
       dqn.reset_noise()  # Draw a new set of noisy weights
 
     action = dqn.act(state)  # Choose an action greedily (with noisy weights)
-    next_state, reward, done, _ = env._step(action)  # Step
+    next_state, reward, done, additional_info = train_env._step(action)  # Step
     if args.reward_clip > 0:
       reward = max(min(reward, args.reward_clip), -args.reward_clip)  # Clip rewards
     mem.append(state, action, reward, done)  # Append transition to memory
+
+    episode_reward += reward
+    episode_reward_history.append(episode_reward)
+    if len(episode_reward_history) > 100:
+      del episode_reward_history[:1]
+    running_reward = np.mean(episode_reward_history)
+    total_cleared_lines += additional_info["cleared_lines"]
 
     # Train and test
     if T >= args.learn_start:
@@ -177,6 +235,13 @@ else:
       #   avg_reward, avg_Q = test(args, T, dqn, val_mem, metrics, results_dir)  # Test
       #   log('T = ' + str(T) + ' / ' + str(args.T_max) + ' | Avg. reward: ' + str(avg_reward) + ' | Avg. Q: ' + str(avg_Q))
       #   dqn.train()  # Set DQN (online network) back to training mode
+
+      if T % eval_interval == 0:
+        dqn.eval()  # Set DQN (online network) to evaluation mode
+        avg_return = compute_avg_return(dqn, test_env, num_eval_episodes)
+        with test_summary_writer.as_default():
+          tf.summary.scalar('avg return', avg_return, step=T)
+        dqn.train()  # Set DQN (online network) back to training mode
 
       #   # If memory path provided, save it
       #   if args.memory is not None:
